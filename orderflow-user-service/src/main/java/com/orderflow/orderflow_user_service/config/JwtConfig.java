fa1e1243f06f9012ee84969db4eafb469af8a221
AlainGZ <devalgz955@gmail.com>
2026-01-06 17:23:14 -0500

    feat: Se configura el JwtUtil

--- orderflow-user-service/src/main/java/com/orderflow/orderflow_user_service/config/JwtConfig.java
+++ orderflow-user-service/src/main/java/com/orderflow/orderflow_user_service/config/JwtConfig.java
@@ -1,4 +1,13 @@
 package com.orderflow.orderflow_user_service.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class JwtConfig {
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 }
